b00f672a35f49e6e6024f0ab2df1457b17f8e4f3
Rian Rodrigues Gomes <rianrgomes@gmail.com>
2025-01-19 13:05:04 -0300

    fix: validate inputs before sending to api

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,8 +16,8 @@ export class HomeComponent {
   constructor(private http: HttpService, private fb: FormBuilder, private router: Router){
     this.form = this.fb.group({
       username: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required]
+      email: ['', Validators.email],
+      password: ['', Validators.minLength(6)]
     })
   }
 
@@ -25,24 +25,24 @@ export class HomeComponent {
   }
 
   onSubmit(){
-    this.errorClass = true;
     if(this.form.invalid){
+      this.errorClass = true;
       if(!this.form.value.username) this.errorMsg = 'You need to choose a username';
       else if(!this.form.value.email.includes('@') || !this.form.value.email.includes('.')) this.errorMsg = 'Type a valid email';
       else if(this.form.value.password.length < 6) this.errorMsg = 'Password must contain at least 6 characters';
     } else{
       this.errorClass = false;
-    }
-
-    this.http.registerStudent(this.form.value).subscribe({
-      next: r => {if(r) this.router.navigateByUrl('/main')},
-      error: e=> {
-        if(!this.errorClass){
-          this.errorMsg = e.error.message
-          this.errorClass = true;
+      
+      this.http.registerStudent(this.form.value).subscribe({
+        next: r => this.router.navigateByUrl('/main'),
+        error: e=> {
+          if(!this.errorClass){
+            this.errorMsg = e.error.message
+            this.errorClass = true;
+          }
         }
-      }
-    })                  
+      })                  
+    }
     
   }
 }
