f93b025e4cb8a1cfbd924a527af3ce6b3b8a7459
Rian Rodrigues Gomes <rianrgomes@gmail.com>
2025-01-20 21:30:02 -0300

    feat: set note id to notes recently createds

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -38,12 +38,14 @@ export class MainComponent {
   }
 
   onSubmit(){
-
-    
-
-    this.notes.push(this.noteForm.value)
-    this.http.registerNote(this.noteForm.value).subscribe(m =>{
-      console.log(m)
+    this.http.registerNote(this.noteForm.value).subscribe((r: any) =>{
+      this.notes.push({
+        id: r.id,
+        title: this.noteForm.value.title,
+        content: this.noteForm.value.content,
+        isPrivate: this.noteForm.value.isPrivate,
+        createdAt: new Date(),
+      })
     })
   }
 
