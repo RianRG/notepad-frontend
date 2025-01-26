7ccdb9e1dd449c12cccf14d4fe862ab5f6ffff5b
Rian Rodrigues Gomes <rianrgomes@gmail.com>
2025-01-26 13:16:04 -0300

    fix: change rendermode from routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { routes, serverRoutes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/authInterceptor';
-import { LoadingInterceptor } from './interceptors/loading.interceptor';
+import { provideServerRendering } from '@angular/platform-server';
+import { provideServerRoutesConfig } from '@angular/ssr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideServerRendering(),
+    provideServerRoutesConfig(serverRoutes),
     provideRouter(routes), 
     provideClientHydration(withEventReplay()), 
     provideHttpClient(withFetch()),
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    // { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,15 @@ import { MainComponent } from './components/main/main.component';
 import { LoginComponent } from './components/login/login.component';
 import { authenticateGuard } from './guards/authenticate.guard';
 import { ProfileComponent } from './components/profile/profile.component';
+import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'register', component: HomeComponent },
   { path: 'main', component: MainComponent, canActivate: [authenticateGuard] },
   { path: 'login', component: LoginComponent },
-  { path: 'profile/:username', component: ProfileComponent, canActivate: [authenticateGuard] }
 ];
+
+export const serverRoutes: ServerRoute[] = [
+  { path: 'profile/:username', renderMode: RenderMode.Server,  }
+]
