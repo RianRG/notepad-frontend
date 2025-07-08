14bd3d14e73fec6a70dee62abafadc34c2496c89
Rian Gomes <rianrgomes@gmail.com>
2025-07-08 20:54:30 -0300

    feat: update backend url

--- environment.ts
+++ environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: process.env['NODE_ENV'] === "production" ? 'https://notepad-jnn2.onrender.com' : "http://localhost:5000"
+  apiUrl: process.env['NODE_ENV'] === "production" ? 'https://ld-sight-singer-emperor.trycloudflare.com' : "http://localhost:5000"
 }
