9b618332533c7dd9adf692fea24b7910bc5dc756
Rian Gomes <rianrgomes@gmail.com>
2025-07-08 21:03:51 -0300

    feat: update backend url again (cloudflared tunnels)

--- environment.ts
+++ environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: process.env['NODE_ENV'] === "production" ? 'https://ld-sight-singer-emperor.trycloudflare.com' : "http://localhost:5000"
+  apiUrl: process.env['NODE_ENV'] === "production" ? 'https://beatles-soldiers-managed-phase.trycloudflare.com' : "http://localhost:5000"
 }
